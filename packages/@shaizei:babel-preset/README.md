6d112dc943366b954b51af35d17fac08cd6151f6
Shahzaib Khalid <shahzaibkhalidmalik@outlook.com>
2019-03-01 03:11:56 +0500

    (babel-preset) - added initial package

---
+++ packages/@shaizei:babel-preset/README.md
@@ -0,0 +1,55 @@
+<h1 align="center">@shaizei/babel-preset</h1>
+
+<p align="center">This package contains shareable babel.js configuration used by the applications created with @shaizei/cli.</p>
+
+## Getting Started
+
+### 1. Installation:
+
+Using Yarn:
+
+```shell
+  yarn add @shaizei/babel-preset --dev
+```
+
+Using npm:
+
+```shell
+  npm install @shaizei/babel-preset --save-dev
+```
+
+### 2. Usage:
+
+* Create a new `.babelrc` file
+* Add `@shaizei/babel-preset` as follows:
+
+For JavaScript projects:
+
+```javascript
+  {
+    "presets": "@shaizei/babel-preset"
+  }
+```
+
+For TypeScript projects:
+
+If you're using this preset in a TypeScript project and want Babel to transpile your TypeScript code, then pass the `typescript` option as follows:
+
+```javascript
+  {
+    "presets": [
+      "@shaizei/babel-preset",
+      {
+        typescript: true
+      }
+    ]
+  }
+```
+
+## Override Default Config
+
+If you want to override the default configuration, then extend `.babelrc` normally.
+
+## Naming Convention
+
+Note that you can also create `babel.config.js` instead of `.babelrc` file.
---
+++ packages/@shaizei:babel-preset/index.js
@@ -0,0 +1,105 @@
+const shaizeiBabelPreset = (context, options = {}) => {
+  
+  const env = process.env.BABEL_ENV || process.env.NODE_ENV;
+
+  const browserlistDev = [
+    'last 2 chrome versions',
+    'last 2 firefox versions',
+    'last 2 edge versions'
+  ];
+
+  const browserlistProd = [
+    '>1%',
+    'last 4 versions',
+    'Firefox ESR',
+    'not ie < 1'
+  ];
+
+  const plugins = [
+    require('react-hot-loader/babel'),
+    [
+      require.resolve('babel-plugin-emotion'),
+      {
+        sourceMap: true,
+        autoLabel: env === 'development',
+        labelFormat: '[local]',
+        cssPropOptimization: true,
+      }
+    ],
+    [
+      require.resolve('@babel/plugin-proposal-class-properties'),
+      {
+        loose: false,
+      },  
+    ],
+    [
+      require.resolve('@babel/plugin-proposal-object-rest-spread'),
+      {
+        loose: false,
+        useBuiltIns: false,
+      },  
+    ],
+    require.resolve('@babel/plugin-syntax-dynamic-import'),
+  ];
+
+  const presets = [
+    [
+      require.resolve('@babel/preset-react'),
+      {
+        pragma: 'React.createElement',
+        pragmaFrag: 'React.Fragment',
+        useBuiltIns: false,
+        development: env === 'development' ? true : false,
+        throwIfNamespace: true,
+      },  
+    ],
+    [
+      require.resolve('@babel/preset-env'),
+      {
+        targets: env === 'production' ? browserlistProd : browserlistDev,
+        spec: false,
+        loose: false,
+        modules: false,
+        debug: false,
+        useBuiltIns: 'usage',
+        forceAllTransforms: false,
+        configPath: process.cwd(),
+        ignoreBrowserslistConfig: true,
+        shippedProposals: true,  
+      }
+    ],
+  ];
+
+  if (options.typescript && options.typescript === true) {
+    presets.push(require.resolve('@babel/preset-typescript'))
+  }
+
+  if (env === 'production') {
+    plugins.push(
+      [
+        require.resolve('@babel/plugin-transform-runtime'),
+        {
+          corejs: false,
+          helpers: true,
+          regenerator: true,
+          useESModule: true,
+        }
+      ],
+      [
+        require.resolve('babel-plugin-transform-react-remove-prop-types'),
+        {
+          mode: 'remove',
+          removeImport: true,
+          ignoreFileNames: ['node_modules'],
+        },    
+      ]
+    )
+  }
+
+  return {
+    plugins,
+    presets
+  }
+};
+
+module.exports = shaizeiBabelPreset;
