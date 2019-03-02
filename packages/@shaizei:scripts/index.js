8a7a145af9f4ccdf5e62a95a74c57aef82fe47a5
Shahzaib Khalid <shahzaibkhalidmalik@outlook.com>
2019-03-02 22:56:15 +0500

    (scripts) - exported new scripts 🎉

--- packages/@shaizei:scripts/index.js
+++ packages/@shaizei:scripts/index.js
@@ -2,7 +2,11 @@ const develop = require('./scripts/develop');
 const lint = require('./scripts/lint');
 const serve = require('./scripts/serve');
 const build = require('./scripts/build');
-const analyze = require('./scripts/analyze')
+const analyze = require('./scripts/analyze');
+const eslintPrettierIntegration = require('./scripts/eslint-prettier-integration');
+const prettier = require('./scripts/prettier');
+const lintFix = require('./scripts/lint-fix');
+const prettierFix = require('./scripts/prettier-fix');
 
 module.exports = {
   lint,
@@ -10,4 +14,8 @@ module.exports = {
   build,
   develop,
   analyze,
+  eslintPrettierIntegration,
+  prettier,
+  lintFix,
+  prettierFix
 };
