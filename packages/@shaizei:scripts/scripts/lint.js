ce3a43acc313f891a2e7bde2501d82b01d8cede6
Shahzaib Khalid <shahzaibkhalidmalik@outlook.com>
2019-03-02 22:57:18 +0500

    (scripts) - fixed a bug with lint script 🐛

--- packages/@shaizei:scripts/scripts/lint.js
+++ packages/@shaizei:scripts/scripts/lint.js
@@ -2,7 +2,7 @@ const lint = () => {
   const { spawn } = require("child_process");
 
   spawn(
-    "./node_modules/@shaizei/eslint-config/node_modules/.bin/eslint --ext .js --ext .jsx --ext .ts --ext .tsx 'src/**'",
+    "./node_modules/@shaizei/eslint-config/node_modules/.bin/eslint --ext .js --ext .jsx --ext .ts --ext .tsx 'src/'",
     {
       shell: true,
       stdio: "inherit"
