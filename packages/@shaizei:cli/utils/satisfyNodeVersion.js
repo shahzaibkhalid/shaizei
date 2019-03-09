408467577ca1bd3bbd25ebb5864c1a4dee7f4298
Shahzaib Khalid <shahzaibkhalidmalik@outlook.com>
2019-03-10 04:35:38 +0500

    (cli) - Node version check before app creation ☝️

---
+++ packages/@shaizei:cli/utils/satisfyNodeVersion.js
@@ -0,0 +1,22 @@
+const chalk = require('chalk');
+
+const currentNodeVersion = process.versions.node;
+const semver = currentNodeVersion.split('.');
+const major = semver[0];
+
+const satisfyNodeVersion = () => {
+  if (major < 10) {
+    console.error(
+      chalk.red(
+        'You are running Node ' +
+          currentNodeVersion +
+          '.\n' +
+          '@shaizei/cli requires Node 10 or higher. \n' +
+          'Please update your version of Node.'
+      )
+    );
+  };
+  process.exit(1);
+};
+
+module.exports = satisfyNodeVersion;
