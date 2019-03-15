d4640a14740f553b172aa98537ddfcb604602977
Shahzaib Khalid <shahzaibkhalidmalik@outlook.com>
2019-03-15 16:14:20 +0500

    (eslint-config) - remove rules

--- packages/@shaizei:eslint-config/utils/rules.js
+++
@@ -1,9 +0,0 @@
-const rules = {
-  'emotion/jsx-import': 'error',
-  'emotion/no-vanilla': 'error',
-  'emotion/import-from-emotion': 'error',
-  'emotion/styled-import': 'error',
-  'emotion/syntax-preference': [2, 'object'],
-};
-
-module.exports = rules;
